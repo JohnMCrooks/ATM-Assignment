9a1f4cc839c3173f791e51f915fc8ee9a1709885
John Crooks <johnmcrooks@gmail.com>
2016-05-23 22:43:11 -0400

    Assignment from day 4 - Removed unused User Class

--- src/com/crooks/Account.java
+++ src/com/crooks/Account.java
@@ -8,27 +8,44 @@ import java.util.HashMap;
 public class Account {
     static String name;
     double balance = 100;
-    HashMap<String, Double> accounts = new HashMap<String, Double>();
+
     static private final String adminPass = "TIY2016";
 
     public Account() {
     }
 
+    public void login() throws Exception {
+        System.out.println("Would you like to login? [y/n]");
+        String temp = Main.scanner.next();
+
+        Main.accounts.put("john", 200.0);
+        Main.accounts.put("alice", 100.0);
+        Main.accounts.put("bob", 134.56);
+        Main.accounts.put("charlie", 45.0);
+
+        if (temp.equals("y")){
+            while (!Main.logout) {
+                setName();
+                atmMenu();
+            }
+        }else {
+            System.out.println("ATM Shutting Down...");
+        }
+
+    }
+
     public void setName(){
-        accounts.put("john", 200.0);
-        accounts.put("alice", 100.0);
-        accounts.put("bob", 134.56);
-        accounts.put("charlie", 45.0);
+
 
         System.out.println("Who are we serving today?");
         String nameInput = Main.scanner.next();
         this.name = nameInput;
 
-        if (accounts.containsKey(nameInput)){
-            System.out.println("welcome " + nameInput + " you have a balance of : $" + accounts.get(nameInput));
-        }else if ((!accounts.containsKey(nameInput))){
-            System.out.println("It appears as though you don't have an account...\n if you open one with us We'll give you a few bucks to get you started.  [y/n]\n");
-                accounts.put(nameInput, 15.0);
+        if (Main.accounts.containsKey(nameInput)){
+            System.out.println("welcome " + nameInput + " you have a balance of : $" + Main.accounts.get(nameInput));
+        }else if ((!Main.accounts.containsKey(nameInput))){
+            System.out.println("It appears as though you don't have an account...\n if you open one with us We'll give you a few bucks to get you started.\n");
+                Main.accounts.put(nameInput, 15.0);
                 System.out.println("welcome " + nameInput + "!\n");
         }
     }
@@ -57,14 +74,41 @@ public class Account {
             throw new Exception("That's not an option! Select option 1,2,3,4, or 5");
         }
     }
+
     public void printBalance() {
-        System.out.println(name + ", Your balance is $" + accounts.get(name) + ".\n");
+        System.out.println(name + ", Your balance is $" + Main.accounts.get(name) + ".\n");
     }
+
+
+    public void withdrawModule() throws Exception {
+        System.out.println("So, how much are you trying to withdraw?\n");
+        String cashRequest = Main.scanner.next();
+
+        //receive users request
+        int temp = Integer.valueOf(cashRequest);
+        double tempaccount = Main.accounts.get(name);
+
+        //Check to make sure user has enough cash to make withdraw
+        if ((double) temp < tempaccount && temp > 0){
+
+            System.out.println("Your cash is in being ejected now, Don't spend it all in one place.\n");
+            System.out.println("Old balance: " + tempaccount + "\n");
+
+            //update the balance
+            this.balance = (tempaccount - temp);
+            System.out.println("New Balance: " + (balance) + "\n");
+            Main.accounts.put(name,balance);
+            atmMenu();
+
+        } else { throw new Exception("Nice try, but you don't have that much!");
+        }
+    }
+
     public void adminMenu() throws Exception {
         System.out.println("Enter the Password");
         String temp = Main.scanner.next();
         if (temp.equals(adminPass)){
-            System.out.println("current accounts include: " + accounts);
+            System.out.println("current accounts include: " + Main.accounts);
             atmMenu();
         } else{
             System.out.println("you aren't allowed here");
@@ -79,8 +123,8 @@ public class Account {
 
         if (confirm.equalsIgnoreCase("y")){
             System.out.println("Alrighty, goodluck.");
-            accounts.remove(name);
-            System.out.println(accounts);
+            Main.accounts.remove(name);
+            System.out.println(Main.accounts);
         }
         else if(confirm.equalsIgnoreCase("n")){
             atmMenu();
@@ -90,28 +134,6 @@ public class Account {
         }
     }
 
-    public void withdrawModule() throws Exception {
-        System.out.println("So, how much are you trying to withdraw?\n");
-        String cashRequest = Main.scanner.next();
-
-        //receive users request
-        int temp = Integer.valueOf(cashRequest);
-        double tempaccount = accounts.get(name);
-
-        //Check to make sure user has enough cash to make withdraw
-        if ((double) temp < tempaccount && temp > 0){
-
-            System.out.println("Your cash is in being ejected now, Don't spend it all in one place.\n");
-            System.out.println("Old balance: " + tempaccount + "\n");
-
-            //update the balance
-            this.balance = (tempaccount - temp);
-            System.out.println("New Balance: " + (balance) + "\n");
-            atmMenu();
-            accounts.put(name,balance);
-        } else { throw new Exception("Nice try, but you don't have that much!");
-        }
-    }
 
     public void exitATM() throws Exception {
         System.out.println("Are you sure you want to Sign out?  [y/n]");
@@ -125,18 +147,5 @@ public class Account {
             throw new Exception("Invalid Input!");
         }
     }
-    public void login() throws Exception {
-        System.out.println("Would you like to login? [y/n]");
-        String temp = Main.scanner.next();
 
-        if (temp.equals("y")){
-            while (!Main.logout) {
-                setName();
-                atmMenu();
-            }
-        }else {
-            System.out.println("ATM Shutting Down...");
-        }
-
-    }
 }
--- src/com/crooks/Main.java
+++ src/com/crooks/Main.java
@@ -1,9 +1,10 @@
 package com.crooks;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
-
+    static HashMap<String, Double> accounts = new HashMap<String, Double>();
     static Scanner scanner = new Scanner(System.in);
     static boolean logout = false;
     public static void main(String[] args) throws Exception {
